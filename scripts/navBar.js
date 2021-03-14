dced43f8405bfeef00ec64ee28aacee27c875c18
Adam Rychert <adamry2000@gmail.com>
2021-03-14 17:35:15 +0100

    ADD scrypt for mobile nav menu

--- scripts/navBar.js
+++ scripts/navBar.js
@@ -1,15 +1,33 @@
-const nav = document.querySelector('.nav__list');
-const burger = document.querySelector('.burger');
-burger.addEventListener('click',() => {
-    nav.classList.toggle('hidden');
+const navBar = document.querySelector('.nav');
+const menuBtn = document.querySelector('.nav__btn');
+const navList = document.querySelector('.nav__list');
+const navItems = document.querySelectorAll('.nav__item');
+let menuOpn = false;
+
+menuBtn.addEventListener('click', () => {
+    if (!menuOpn) {
+        menuBtn.classList.add('open');
+        navList.style.display = "flex";
+        menuOpn = true;
+    } else {
+        menuBtn.classList.remove('open');
+        navList.style.display = "none";
+        menuOpn = false;
+    }
+})
+
+
+navList.addEventListener('click', () => {
+    menuBtn.classList.remove('open');
+    navList.style.display = "none";
+    menuOpn = false;
 })
 
 const arrow = document.querySelector('.arrow');
-
-window.onscroll = function (){
+window.onscroll = function () {
     if (document.documentElement.scrollTop > 90) {
         arrow.classList.remove('hidden');
-      } 
+    }
 }
 arrow.addEventListener('click', () => {
     document.documentElement.scrollTop = 0;
